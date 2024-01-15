1b5d50596af9d71cf4e5fcc89163d388bab3140a
CarrotDLaw <dannylaw0514@gmail.com>
2024-01-16 03:17:20 +0800

    feat: ✨ update file

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -25,17 +25,19 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
   disabledDates,
 }) => {
   return (
-    <div className="bg-white  rounded-xl  border-[1px] border-neutral-200 overflow-hidden">
+    <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
       <div className="flex flex-row items-center gap-1 p-4">
         <div className="text-2xl font-semibold">$ {price}</div>
         <div className="font-light text-neutral-600">night</div>
       </div>
       <hr />
-      <Calendar
-        value={dateRange}
-        disabledDates={disabledDates}
-        onChange={(value) => onChangeDate(value.selection)}
-      />
+      <div className="flex flex-row justify-center">
+        <Calendar
+          value={dateRange}
+          disabledDates={disabledDates}
+          onChange={(value) => onChangeDate(value.selection)}
+        />
+      </div>
       <hr />
       <div className="p-4">
         <Button disabled={disabled} label="Reserve" onClick={onSubmit} />
